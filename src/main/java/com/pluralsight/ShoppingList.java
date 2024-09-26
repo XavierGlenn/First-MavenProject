523ca1777fa17271c8e0840c8bce4720d975b4b6
Xavier Glenn <xglenn@appdev.yearup.org>
2024-09-26 13:36:56 -0700

    completed ShoppingList app

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main ( String[] args ) {
+        System.out.println("Milk, potatoes, eggs, bacon, bread, orange juice, apple juice, cereal, strawberries, raspberries");
+    }
+}
