9cd523d6faee293f3103f0eca05a08d6bf8f3d04
Xavier Glenn <xglenn@appdev.yearup.org>
2024-09-26 14:11:40 -0700

    completed ShoppingList app

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -1,7 +1,12 @@
 package com.pluralsight;
 
 public class ShoppingList {
-    public static void main ( String[] args ) {
-        System.out.println("Milk, potatoes, eggs, bacon, bread, orange juice, apple juice, cereal, strawberries, raspberries");
+   public static void main ( String[] args ) {
+        System.out.println("Hello world!");
+
+        String shoppingItem1 = ("Milk");
+        String[] shoppingList = {"Bacon", "Eggs", "Biscuits", "Gravy", "Strawberries", "Flour", "Apple Juice", "Orange Juice", "Cereal", "Butter"};
+        System.out.println(shoppingItem1);
+        System.out.println(Arrays.shoppingList);
     }
 }
